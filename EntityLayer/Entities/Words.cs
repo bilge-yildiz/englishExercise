8ae758f4bdbbb762b58927707af0ef8aca017831
yildizzbilge <yildizzbilge@gmail.com>
2022-02-16 19:55:46 +0300

    Add project files.

---
+++ EntityLayer/Entities/Words.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Words
+    {
+        public int WordId {get ;set;} 
+
+        public string WordEng { get; set; }
+        public string WordTur { get; set; }
+        public int Level { get; set; }
+        
+        public bool IsActive { get; set; }
+    }
+}
---
+++ EntityLayer/Entities/WrongWords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+       public class WrongWords
+    {
+        public int WrongWordsId { get; set; }
+
+        public int WordId { get; set; }
+
+        public int UserId { get; set; }
+        public int Count { get; set; }
+
+    }
+
+}
